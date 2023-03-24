80294a6d6b6960f957d40edfbe54a27bc4a343ac
HafizhohViarma <127370927+HafizhohViarma@users.noreply.github.com>
2023-03-24 22:17:37 +0700

    Latihan-modul3

---
+++ HafizhohViarma.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PBO_2201081007;
+
+/**
+ *
+ * @author HP 840 G3
+ */
+public class HafizhohViarma {
+    public static void main (String[] args){
+        System.out.println("Welcome to Java Programming Hafizhoh Viarma!!!");
+    }
+    
+}
---
+++ TheTree.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PBO_2201081007;
+
+/**
+ *
+ * @author HP 840 G3
+ */
+public class TheTree {
+    public static void main(String[] args){
+        System.out.println("I think that I shall never see,");
+        System.out.println("a poem as lovely as a tree.");
+        System.out.println("A tree whose hungry mouth is pressed");
+        System.out.println("Against the Earth’s sweet flowing breast.");
+    }
+}
