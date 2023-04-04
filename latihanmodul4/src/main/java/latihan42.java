db340312296c3f42458529137c0f9271c9739b5c
HafizhohViarma <127370927+HafizhohViarma@users.noreply.github.com>
2023-04-04 10:41:37 +0700

    Add files via upload

---
+++ latihanmodul4/src/main/java/latihan42.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP 840 G3
+ */
+public class latihan42 {
+    public static void main(String[] args){
+        int number1=10;
+        int number2=20;
+        int number3=45;
+        int fathy=(number1+number2+number3)/3;
+        
+        System.out.println("number1="+number1);
+        System.out.println("number2="+number2);
+        System.out.println("number3="+number3);
+        System.out.println("Avarage is="+fathy);
+    }
+    
+}
